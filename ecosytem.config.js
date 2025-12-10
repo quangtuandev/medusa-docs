8d11aecd7d158b4fc0725b25fdf34021b14a2683
DuanYu <duanyu-fs@192.168.2.69>
2025-12-10 10:34:48 +0700

    rebuild

--- ecosytem.config.js
+++ ecosytem.config.js
@@ -1,14 +1,13 @@
 module.exports = {
     apps: [
-      {
-        name: 'user-guide',
-        script: 'yarn',
-        args: 'start:monorepo -p 3000',
-        autorestart: true,
-         watch: false,
-         max_memory_restart: '1G',
-         interpreter: "/bin/bash",
-      }
-    ],
-  };
-  
+        {
+            name: "user-guide",
+            script: "yarn",
+            args: "start:monorepo -p 3000",
+            interpreter: "/usr/bin/node",
+            env: {
+                PATH: "/usr/local/bin:/usr/bin:/bin"
+            }
+        }
+    ]
+}
